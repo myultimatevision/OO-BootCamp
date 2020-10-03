eedf6def18b7ce29d8826e5ca34202018ee91574
nagendra <=>
2020-10-03 12:47:01 +0530

    implemented isFilled method in ParkingLot Class

--- src/main/java/parking/ParkingLot.java
+++ src/main/java/parking/ParkingLot.java
@@ -4,18 +4,21 @@ public class ParkingLot {
     private final int[] spaces;
     private int nextFreeSpacePosition;
 
-    public ParkingLot(int noOFSpaces) {
+    public ParkingLot(int noOfSpaces) {
         this.nextFreeSpacePosition = 0;
-        this.spaces = new int[noOFSpaces];
+        this.spaces = new int[noOfSpaces];
+    }
+
+    public boolean isFilled() {
+        return this.nextFreeSpacePosition == this.spaces.length;
     }
 
     public int park(int carNumber) throws SpaceNotAvailableException {
-        if (nextFreeSpacePosition == spaces.length) {
+        if (this.isFilled()) {
             throw new SpaceNotAvailableException("No Space is Available");
         }
 
-        this.spaces[nextFreeSpacePosition] = carNumber;
-
-        return nextFreeSpacePosition++;
+        this.spaces[this.nextFreeSpacePosition] = carNumber;
+        return this.nextFreeSpacePosition++;
     }
 }
--- src/test/java/parking/ParkingLotTest.java
+++ src/test/java/parking/ParkingLotTest.java
@@ -2,21 +2,46 @@ package parking;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingLotTest {
     @Test
     void shouldParkTheCarWhenThereIsASpaceInParkingLot() throws SpaceNotAvailableException {
-        ParkingLot parkingLot = new ParkingLot(8);
+        ParkingLot parkingLot = new ParkingLot(10);
 
-        assertEquals(0, parkingLot.park(1234));
+        parkingLot.park(1234);
+        parkingLot.park(1235);
+
+        assertEquals(2, parkingLot.park(1235));
     }
 
     @Test
-    void shouldNotParkTheCarWhenThereIsNoSPace() {
-        ParkingLot parkingLot = new ParkingLot(0);
+    void shouldNotParkTheCarWhenThereIsNoSPace() throws SpaceNotAvailableException {
+        ParkingLot parkingLot = new ParkingLot(2);
+
+        parkingLot.park(1234);
+        parkingLot.park(1235);
 
         assertThrows(SpaceNotAvailableException.class, () -> parkingLot.park(2345));
+    }
+
+    @Test
+    void shouldGiveTrueWhenTheParkingLotIsFilled() throws SpaceNotAvailableException {
+        ParkingLot parkingLot = new ParkingLot(2);
+
+        parkingLot.park(1234);
+        parkingLot.park(1235);
+
+        assertTrue(parkingLot.isFilled());
+    }
+
+    @Test
+    void shouldGiveFalseWhenTheParkingLotIsNotFilled() throws SpaceNotAvailableException {
+        ParkingLot parkingLot = new ParkingLot(3);
+
+        parkingLot.park(1234);
+        parkingLot.park(1235);
+
+        assertFalse(parkingLot.isFilled());
     }
 }
